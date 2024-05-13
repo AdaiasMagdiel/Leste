b4b321bbff21ebe685b3531c5b7ebb22939bb6a5
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-13 15:03:32 -0300

    feat: add command line interface for running tests
    - Added CLI functionality for handling command line flags and options
    - Added logic to set Leste verbosity and exit behavior based on command line flags
    - Implemented placeholders for upcoming features:
      - Ability to specify a tests folder from the command line
      - Logic to iterate over test files starting with 'test'
      - Placeholder logic for collecting and executing test functions using Leste

---
+++ leste/main.lua
@@ -0,0 +1,19 @@
+local Leste = require("leste.leste")
+local fs = require("leste.utils.fs")
+local cli = require("leste.utils.cli")
+
+local LesteCLI = {}
+
+LesteCLI.main = function()
+	local flags = cli.getFlags(true)
+
+	Leste.verbose = flags.verbose
+	Leste.exitOnFirst = flags.exitOnFirst
+
+	-------------------------------------
+	-- @todo: Get the tests folder from the command line, if none is passed, default to 'tests/'
+	-- @todo: Iterate over all files that start with 'test' (e.g.: testLeste.lua or test_leste.lua)
+	-- @todo: Collect all Leste.it functions from these files and execute all of them using Leste.run (I don't know how I will do this, but I will discover it) 
+end
+
+LesteCLI.main()
