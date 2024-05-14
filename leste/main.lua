5df500c8daa7d4f42d170da7180e3c520cf9a2dd
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-13 21:34:38 -0300

    feat: update LesteCLI program usage with folder option
    - Added program and usage details to LesteCLI for better understanding and usage.
    - Updated the option to specify a folder for finding tests in the program.
    - Implemented the capability to load tests from all files in the specified folder and add them to `Leste.tests`.
    - Updated handling for displaying usage based on the number of arguments passed.
    - Enhanced the process of running tests with `Leste.run()`.

--- leste/main.lua
+++ leste/main.lua
@@ -4,20 +4,50 @@ local cli = require("leste.utils.cli")
 
 local LesteCLI = {}
 
+LesteCLI.program = "lua leste/main.lua"
+LesteCLI.usage = ([[
+Usage: %s [options] [folder]
+
+Arguments:
+    folder                 folder to find tests, default is "./tests"
+
+Options:
+    -v, --verbose          display output of print function inside tests, default false
+    -x, --exitfirst        exit on first failure, default false
+]]):format(LesteCLI.program)
+
 LesteCLI.main = function()
 	local flags = cli.getFlags(true)
+	local testsFolder = "tests"
+	local filePrefix = "test"
 
+	-- update the Leste configs
 	Leste.verbose = flags.verbose
 	Leste.exitOnFirst = flags.exitOnFirst
 
-	for _, file in ipairs(fs.listDir()) do
-		print(file)
+	-- get the tests folder or print the usage
+	if #arg == 1 then
+		testsFolder = arg[1]
+	elseif #arg > 1 then
+		print(LesteCLI.usage)
 	end
 
-	-------------------------------------
-	-- @todo: Get the tests folder from the command line, if none is passed, default to 'tests/'
-	-- @todo: Iterate over all files that start with 'test' (e.g.: testLeste.lua or test_leste.lua)
-	-- @todo: Collect all Leste.it functions from these files and execute all of them using Leste.run (I don't know how I will do this, but I will discover it) 
+	-- apparently, with this code I can load the tests of all files
+	-- and put to the `Leste.tests`.
+	local path = fs.path() .. fs.sep() .. testsFolder .. fs.sep()
+	for _, file in ipairs(fs.listDir(path)) do
+		if file:sub(1, #filePrefix) == filePrefix then
+			-- save the actual file name to use when `Leste.it` will be triggered.
+			Leste.actualFile = file
+			dofile(path .. file)
+
+			-- reset
+			Leste.actualFile = ""
+		end
+	end
+
+	-- finally, run the tests. Is that all?
+	Leste.run()
 end
 
 LesteCLI.main()
