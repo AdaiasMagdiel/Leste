5d8a85868e9b752258bbd523df02e92d1ada32ad
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-14 13:13:13 -0300

    feat: modify run method to execute tests
    - Added a new method `run` to the `D` object in `dummy_leste.lua`
    - The `run` method enhances the testing functionality by iterating over tests and handling assertions
    - The `run` method temporarily overrides the `assert` function to track assertions made during test execution

--- tests/dummy_leste.lua
+++ tests/dummy_leste.lua
@@ -5,6 +5,20 @@ local file = fs.readFile(path)
 
 local D = load(file)()
 
+D.run = function()
+	local assertTemp = assert
+    assert = function(...)
+    	D.assertions = D.assertions + 1
+        assertTemp(...)
+    end
+
+    for _, test in ipairs(D.tests) do
+        pcall(test.action)
+    end
+
+    assert = assertTemp
+end
+
 D.reset = function()
 	D.assertions 	= 0
     D.verbose 		= false
