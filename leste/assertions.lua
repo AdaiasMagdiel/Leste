9c1057d14319087ad4e16795afdf5b2beb3f246b
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-15 20:05:17 -0300

    feat: improve error message format
    - Updated the error message format in the `equal` assertion function to be more concise and clear.

--- leste/assertions.lua
+++ leste/assertions.lua
@@ -11,7 +11,7 @@ end
 Assertions.equal = function(value1, value2, message)
 	message = message and
 		message or
-		("Assertion failed: Expected %s to be equal %s, but it's false."):
+		("Assertion failed: %s is not equal %s."):
 		format(tostring(value1), tostring(value2))
 
 	assert(value1 == value2, message)
