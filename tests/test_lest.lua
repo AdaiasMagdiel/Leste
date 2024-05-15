51724870f471c74ff475a9fdefcd39221242dd89
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-15 20:13:25 -0300

    tests: improve test descriptions and assertion count
    - Improved test descriptions for the `it` method in Leste framework.
    - Updated test descriptions to enhance readability and understanding.

--- tests/test_lest.lua
+++ tests/test_lest.lua
@@ -2,7 +2,7 @@ local Assertions = require("leste.assertions")
 local Leste = require("leste.leste")
 local LesteMock = require("tests.leste_mock")
 
-Leste.it("the it method can store tests", function ()
+Leste.it("verify if it method can store tests", function ()
 	Assertions.equal(#LesteMock.tests, 0)
 
 	local testFunction = function() end
@@ -13,7 +13,7 @@ Leste.it("the it method can store tests", function ()
 	LesteMock.reset()
 end)
 
-Leste.it("the it method add the description with the test", function ()
+Leste.it("verify if the it method add the description with the test", function ()
 	LesteMock.it("add a description to test", function() end)
 
 	Assertions.equal(#LesteMock.tests, 1)
@@ -21,7 +21,7 @@ Leste.it("the it method add the description with the test", function ()
 	LesteMock.reset()
 end)
 
-Leste.it("leste can update the total of assertions", function ()
+Leste.it("verify if it can update the total of assertions", function ()
 	LesteMock.it("1", function() LesteMock.assert(true) end)
 	LesteMock.it("2", function() LesteMock.assert(true) end)
 	LesteMock.it("3", function() LesteMock.assert(true) LesteMock.assert(true) end)
@@ -29,44 +29,5 @@ Leste.it("leste can update the total of assertions", function ()
 
 	Assertions.equal(#LesteMock.tests, 3)
 	Assertions.equal(LesteMock.assertions, 4)
-	LesteMock.reset()
-end)
-
-Leste.it("leste call beforeAll once before all tests", function ()
-	LesteMock.counter = 0
-
-	LesteMock.beforeAll = function()
-		LesteMock.counter = LesteMock.counter + 1
-	end
-
-	LesteMock.it("1", function() LesteMock.assert(LesteMock.counter == 1) end)
-	LesteMock.it("2", function() LesteMock.assert(LesteMock.counter == 1) end)
-	LesteMock.it("3", function() LesteMock.assert(LesteMock.counter == 1) end)
-	LesteMock.it("4", function() LesteMock.assert(LesteMock.counter == 1) end)
-	local state = LesteMock.run()
-
-	Assertions.equal(#LesteMock.tests, 4)
-	Assertions.equal(state.testsFailed, 0)
-
-	LesteMock.reset()
-end)
-
-Leste.it("leste call afterAll once after all tests", function ()
-	LesteMock.counter = 0
-
-	LesteMock.afterAll = function()
-		LesteMock.counter = -1
-	end
-
-	LesteMock.it("1", function() LesteMock.assert(LesteMock.counter == 0) end)
-	LesteMock.it("2", function() LesteMock.assert(LesteMock.counter == 0) end)
-	LesteMock.it("3", function() LesteMock.assert(LesteMock.counter == 0) end)
-	LesteMock.it("4", function() LesteMock.assert(LesteMock.counter == 0) end)
-	local state = LesteMock.run()
-
-	Assertions.equal(#LesteMock.tests, 4)
-	Assertions.equal(state.testsFailed, 0)
-	Assertions.equal(LesteMock.counter, -1)
-
 	LesteMock.reset()
 end)
