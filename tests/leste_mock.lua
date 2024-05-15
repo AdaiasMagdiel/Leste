5e6162c26c2c692904767b7a04b958607d3b0151
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-14 22:58:41 -0300

    feat: add LesteMock module for test mocking
    - Added a new file `leste_mock.lua` containing functions to mock the `Leste` module for testing purposes.
    - Introduced functions like `assert`, `run`, and `reset` within the `LesteMock` module to replicate essential behaviors of the `Leste` module for testing.

---
+++ tests/leste_mock.lua
@@ -0,0 +1,45 @@
+local fs = require("leste.utils.fs")
+
+-- I need all the stuff to deepcopy the Leste module
+-- otherwise, there may be some collateral effects.
+local path = fs.path() .. fs.sep() .. "leste" .. fs.sep() .. "leste.lua"
+local file = fs.readFile(path)
+
+local LesteMock = load(file)()
+
+-- Similar to the assert modification in Leste.init
+LesteMock.assert = function(...)
+    LesteMock.assertions = LesteMock.assertions + 1
+    LesteMock.refs.assert(...)
+end
+
+-- This mock function aims to replicate the essence of Leste.run
+-- in order to achieve a similar outcome.
+LesteMock.run = function()
+    LesteMock.beforeAll()
+
+    local state = {
+        totalRuntime = 0,
+        testsPassed = 0,
+        testsFailed = 0,
+    }
+
+    for _, test in ipairs(LesteMock.tests) do
+        local result = pcall(test.action)
+
+        state.testsPassed = state.testsPassed + (result and 1 or 0)
+        state.testsFailed = state.testsFailed + (result and 0 or 1)
+    end
+
+    LesteMock.afterAll()
+
+    return state
+end
+
+-- Reset the module
+LesteMock.reset = function()
+    LesteMock.tests = {}
+    LesteMock.assertions = 0
+end
+
+return LesteMock
