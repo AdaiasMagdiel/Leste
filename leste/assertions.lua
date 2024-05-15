98bc3e06423db65dc270bce71bb432ee04237340
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-15 19:19:20 -0300

    feat: add `equal` assertion method
    - Added a new `equal` method to the `Assertions` module for comparing two values.

--- leste/assertions.lua
+++ leste/assertions.lua
@@ -1,8 +1,20 @@
 local Assertions = {}
 
 Assertions.assert = function(condition, message)
-	message = message and message or "Assertion failed: Expected condition to be true, but it's false."
+	message = message and
+		message or
+		"Assertion failed: Expected condition to be true, but it's false."
+
 	assert(condition, message)
+end
+
+Assertions.equal = function(value1, value2, message)
+	message = message and
+		message or
+		("Assertion failed: Expected %s to be equal %s, but it's false."):
+		format(tostring(value1), tostring(value2))
+
+	assert(value1 == value2, message)
 end
 
 return Assertions
