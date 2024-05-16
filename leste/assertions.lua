f607b76c5256b56a642e491a10b555a5ab34235b
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-15 23:49:45 -0300

    feat: add new assertion functions
    - Added `notAssert` function to assert a condition to be false.
    - Modified `equal` function to have clearer error messages and comparisons.
    - Added `notEqual` function to assert expected and actual values not being equal.
    - Added `tableHasKey` function to validate the presence of a key in a table.
    - Added `tableNotHasKey` function to validate the absence of a key in a table.
    - Added `tableContains` function to check if a table contains a specific value.
    - Added `tableNotContains` function to check if a table does not contain a specific value.

--- leste/assertions.lua
+++ leste/assertions.lua
@@ -8,13 +8,82 @@ Assertions.assert = function(condition, message)
 	assert(condition, message)
 end
 
-Assertions.equal = function(value1, value2, message)
+Assertions.notAssert = function(condition, message)
+	message = message and
+		message or
+		"Assertion failed: Expected condition to be false, but it's true."
+
+	assert(not condition, message)
+end
+
+Assertions.equal = function(expected, actual, message)
 	message = message and
 		message or
 		("Assertion failed: %s is not equal %s."):
-		format(tostring(value1), tostring(value2))
+		format(tostring(expected), tostring(actual))
 
-	assert(value1 == value2, message)
+	assert(expected == actual, message)
+end
+
+Assertions.notEqual = function(expected, actual, message)
+	message = message and
+		message or
+		("Assertion failed: %s is equal %s."):
+		format(tostring(expected), tostring(actual))
+
+	assert(expected ~= actual, message)
+end
+
+Assertions.tableHasKey = function(tbl, key, message)
+	message = message and
+		message or
+		("Assertion failed: The table does not contain the key '%s'."):
+		format(tostring(key))
+
+	assert(tbl[key] ~= nil, message)
+end
+
+Assertions.tableNotHasKey = function(tbl, key, message)
+	message = message and
+		message or
+		("Assertion failed: The table contains the key '%s'."):
+		format(tostring(key))
+
+	assert(tbl[key] == nil, message)
+end
+
+Assertions.tableContains = function(tbl, value, message)
+	message = message and
+		message or
+		("Assertion failed: The table does not have the value '%s'."):
+		format(tostring(value))
+
+	local hasValue = false
+	for _, v in pairs(tbl) do
+		if v == value then
+			hasValue = true
+			break
+		end
+	end
+
+	assert(hasValue, message)
+end
+
+Assertions.tableNotContains = function(tbl, value, message)
+	message = message and
+		message or
+		("Assertion failed: The table contains the value '%s'."):
+		format(tostring(value))
+
+	local hasValue = false
+	for _, v in pairs(tbl) do
+		if v == value then
+			hasValue = true
+			break
+		end
+	end
+
+	assert(not hasValue, message)
 end
 
 return Assertions
