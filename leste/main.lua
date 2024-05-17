15d669d2e3cc9fa7701caba7bd08e87045e8656f
Adaías Magdiel <adaiasmagdiell@gmail.com>
2024-05-16 23:50:51 -0300

    feat: enhance CLI functionality
    - Update usage instructions to allow for multiple file/folder inputs
    - Add examples to demonstrate different CLI usage scenarios
    - Introduce functions to load files and folders for testing
    - Improve error handling for missing files or folders
    - Refactor loading logic to handle both single files and folders
    - Enhance the execution flow for running tests

--- leste/main.lua
+++ leste/main.lua
@@ -13,26 +13,56 @@ LesteCLI.program = "leste"
 
 -- @field LesteCLI.usage string Usage instructions for the CLI.
 LesteCLI.usage = ([[
-Usage: %s [options] [folder]
+Usage: %s [options] [file/folder...]
 
 Arguments:
-    folder                 Folder to find tests, default is "./tests"
+	file                   File (or files) with tests to run.
+    folder                 Folder (or folders) to find tests, default is "./tests"
 
 Options:
     -d, --disable-color    Disable color for the output, print raw text
     -h, --help             Show this help message and exit.
     -v, --verbose          Display output of print function inside tests, default false
     -x, --exitfirst        Exit on first failure, default false
+
+Examples:
+    leste -v
+    leste -vx
+    leste --disable-color -x
+    leste -v my-tests-folder
+    leste my_test.lua
+    leste test_file.lua folder-with-tests -vx
 ]]):format(LesteCLI.program)
 
+local function loadFile(file, prefix)
+	-- print('>', file, fs.filename(file))
+
+	if fs.filename(file):sub(1, #prefix) == prefix then
+		-- print('>>')
+		-- save the actual file name to use when `Leste.it` will be triggered.
+		Leste.actualFile = file
+		dofile(file)
+
+		-- reset
+		Leste.actualFile = ""
+	end
+end
+
+local function loadFolder(folder, prefix)
+	local path = fs.path() .. fs.sep() .. folder .. fs.sep()
+
+	for _, file in ipairs(fs.listDir(path)) do
+		loadFile(path .. file, prefix)
+	end
+end
 
 --- Main function to run the Leste testing framework via CLI.
 -- @function LesteCLI.main
 LesteCLI.main = function()
 	cli.parse()
 
-	local testsFolder = "tests"
 	local filePrefix = "test"
+	local testsFolder = "tests"
 
 	-- update the Leste configs
 	Leste.disableColor 	= cli.getFlag('-d', "--disable-color")
@@ -45,29 +75,36 @@ LesteCLI.main = function()
 		os.exit(0)
 	end
 
-	-- get the tests folder or print the usage
-	if #cli.args == 1 then
-		testsFolder = cli.args[1]
-	elseif #cli.args > 1 then
-		print(LesteCLI.usage)
-	end
+	if #cli.args == 0 then
+		if not fs.exists(testsFolder) then
+			print("Error: Unable to locate the './tests' folder.")
+			os.exit(1)
+		else
+			loadFolder(testsFolder, filePrefix)
+			Leste.run()
+		end
+	else
+		local loaded = 0
 
-	-- apparently, with this code I can load the tests of all files
-	-- and put to the `Leste.tests`.
-	local path = fs.path() .. fs.sep() .. testsFolder .. fs.sep()
-	for _, file in ipairs(fs.listDir(path)) do
-		if file:sub(1, #filePrefix) == filePrefix then
-			-- save the actual file name to use when `Leste.it` will be triggered.
-			Leste.actualFile = file
-			dofile(path .. file)
+		for _, argument in ipairs(cli.args) do
+			if not fs.exists(argument) then
+				print("Warning: '"..argument.."' is not found.")
+			else
+				if fs.isFile(argument) then
+					local path = fs.path() .. fs.sep()
+					loadFile(path .. argument, "")
+				else
+					loadFolder(argument, filePrefix)
+				end
 
-			-- reset
-			Leste.actualFile = ""
+				loaded = loaded + 1
+			end
+		end
+
+		if loaded > 0 then
+			Leste.run()
 		end
 	end
-
-	-- finally, run the tests. Is that all?
-	Leste.run()
 end
 
 LesteCLI.main()
